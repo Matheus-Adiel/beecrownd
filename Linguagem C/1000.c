e45878c22646f53b77ac70541845cd3abba021ae
Matheus Adiel <122126818+Matheus-Adiel@users.noreply.github.com>
2023-11-13 19:28:21 -0300

    creat aswer 1000 in c

---
+++ Linguagem C/1000.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    //característica do beecrownd em C é que sempre é necessário pôr
+    // <\n> no final dos prints.
+    printf("Hello World!\n");
+    return 0;
+}
